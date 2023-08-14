d64ce3e47e838240f7468daedec62addd39f34e7
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-08-14 20:24:12 +0530

    header added

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -1,23 +1,26 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from './Theme';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import Icon from '@mui/icons-material/Face3TwoTone';
 
 // Array of page names for the navigation menu
 const pages = ['Experience', 'Projects', 'Contact'];
 
+
+
 // Component for the responsive app bar
 function ResponsiveAppBar() {
+  const theme = useContext(ThemeContext);
+
   // State to manage the anchor element of the navigation menu
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
@@ -32,11 +35,10 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <AppBar position="static" elevation={0} sx={{ backgroundColor: theme.colors.midnight, width: '100vw' }}>
+        <Toolbar disableGutters sx={{ backgroundColor: theme.colors.background, borderRadius: '25px', margin: '1rem', maxWidth: '100%', padding: '0 3rem'}}>
           {/* Logo and title */}
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Icon sx={{ display: { xs: 'none', md: 'flex', fontSize: '2.6rem' }, padding: '1rem', color: theme.colors.peacock }} />
           <Typography
             variant="h6"
             noWrap
@@ -45,14 +47,20 @@ function ResponsiveAppBar() {
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              fontFamily: theme.typography.logoFont,
               fontWeight: 700,
               letterSpacing: '.3rem',
-              color: 'inherit',
               textDecoration: 'none',
+              fontSize: '2rem'
             }}
           >
-            LOGO
+            <span style={{ color: theme.colors.redhot }}>P</span>
+            <span style={{ color: theme.colors.teal, }}>a</span>
+            <span style={{ color: theme.colors.sunray }}>r</span>
+            <span style={{ color: theme.colors.blossom }}>i</span>
+            <span style={{ color: theme.colors.peacock }}>s</span>
+            <span style={{ color: theme.colors.redhot }}>h</span>
+            <span style={{ color: theme.colors.sunray }}>a</span>
           </Typography>
 
           {/* Navigation menu for small screens */}
@@ -65,7 +73,7 @@ function ResponsiveAppBar() {
               onClick={handleOpenNavMenu}
               color="inherit"
             >
-              <MenuIcon />
+              <MenuIcon style={{ color: theme.colors.sunray }} />
             </IconButton>
             <Menu
               id="menu-appbar"
@@ -83,19 +91,23 @@ function ResponsiveAppBar() {
               onClose={handleCloseNavMenu}
               sx={{
                 display: { xs: 'block', md: 'none' },
+                "& .MuiMenu-paper":
+                  { backgroundColor: '#edeae3', },
               }}
             >
               {/* Render each page as a menu item */}
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+              {pages.map((page, index) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu} sx={{ backgroundColor: '#edeae3' }} >
+                  <Typography textAlign="center" sx={{ color: [theme.colors.redhot, theme.colors.sunray, theme.colors.peacock][index % 3] }}>
+                    {page}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
 
           {/* Logo and title for small screens */}
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Icon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, color: theme.colors.peacock }} />
           <Typography
             variant="h5"
             noWrap
@@ -105,31 +117,41 @@ function ResponsiveAppBar() {
               mr: 2,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: 'retro',
               fontWeight: 700,
               letterSpacing: '.3rem',
               color: 'inherit',
               textDecoration: 'none',
             }}
           >
-            LOGO
+            <span style={{ color: theme.colors.redhot }}>P</span>
+            <span style={{ color: theme.colors.teal, }}>A</span>
+            <span style={{ color: theme.colors.sunray }}>R</span>
+            <span style={{ color: theme.colors.blossom }}>I</span>
+            <span style={{ color: theme.colors.peacock }}>S</span>
+            <span style={{ color: theme.colors.redhot }}>H</span>
+            <span style={{ color: theme.colors.sunray }}>A</span>
           </Typography>
 
           {/* Navigation buttons for medium and large screens */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
             {/* Render each page as a button */}
-            {pages.map((page) => (
+            {pages.map((page, index) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{
+                  my: 2,
+                  color: [theme.colors.redhot, theme.colors.sunray, theme.colors.peacock][index % 3], // Add color values for each index
+                  display: 'block',
+                  fontSize: '1rem'
+                }}
               >
                 {page}
               </Button>
             ))}
           </Box>
         </Toolbar>
-      </Container>
     </AppBar>
   );
 }
