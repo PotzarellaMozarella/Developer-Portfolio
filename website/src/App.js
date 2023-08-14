1a6996d7c1178671ce7b44d572115b43304a9624
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-08-14 20:25:14 +0530

    font added

--- website/src/App.js
+++ website/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/Home';
 import './App.css';
-import './fonts/bettynoir.ttf'
+// import './fonts/magicretro.ttf';
+
+import { ThemeProvider } from './components/Theme'; // Import your ThemeProvider here
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-       </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
