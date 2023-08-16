acf21fb04b8395b0d8d48e840b1711e0e6fdc325
Parisha <PARISHA.WORKSHOP@GMAIL.COM>
2023-08-16 17:04:20 +0530

    routes updated

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/Home';
 import Projects from './pages/Projects/Projects';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Experience from './pages/Experience/Experience';
 import './App.css';
 import { ThemeProvider } from './components/Theme'; // Import your ThemeProvider here
 
@@ -13,6 +16,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
